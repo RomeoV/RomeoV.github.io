5ecfdf4d83c9e4571860158fcba34ecdefc26792
Romeo Valentin <romeov@stanford.edu>
2024-01-14 18:04:29 -0800

    Add graph screenshots.

---
+++ tikz-pictures/ksvd-overview/.auctex-auto/main.el
@@ -0,0 +1,17 @@
+(TeX-add-style-hook
+ "main"
+ (lambda ()
+   (setq TeX-command-extra-options
+         "-shell-escape")
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("standalone" "tikz" "convert={density=600,outext=.png}")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "standalone"
+    "standalone10"
+    "tikz"
+    "varwidth")
+   (TeX-add-symbols
+    "nnz"))
+ :latex)
+
